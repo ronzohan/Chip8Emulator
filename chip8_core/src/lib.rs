33faa3cedc313b9643d32d9554633688b9ea55bd
Ron Daryl Magno <ron.zohan@gmail.com>
2022-03-10 09:49:16 +0800

    Add frontend functions

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -62,6 +62,20 @@ impl Emu {
         new_emu
     }
 
+    pub fn get_display(&self) -> &[bool] {
+        &self.screen
+    }
+
+    pub fn keypress(&mut self, idx: usize, pressed: bool) {
+        self.keys[idx] = pressed;
+    }
+
+    pub fn load(&mut self, data: &[u8]) {
+        let start = START_ADDR as usize;
+        let end = (START_ADDR as usize) + data.len();
+        self.ram[start..end].copy_from_slice(data);
+    }
+
     pub fn reset(&mut self) {
         self.pc = START_ADDR;
         self.ram = [0; RAM_SIZE];
